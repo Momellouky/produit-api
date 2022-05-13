4d6a93aa7adc498b4b288374e551fe043fc149b6
= <=>
2022-05-13 15:20:44 +0100

    feat: 404 not found status code
    respond with 404 status code when the database is empty.

---
+++ produit/src/main/java/com/nawratech/back/produit/productAdvice/productAdvice.java
@@ -0,0 +1,22 @@
+package com.nawratech.back.produit.productAdvice;
+
+import com.nawratech.back.produit.exceptionHandlers.RessourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class productAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(RessourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String productNotFound(RessourceNotFoundException ex) {
+        return ex.getMessage(); 
+    }
+}
